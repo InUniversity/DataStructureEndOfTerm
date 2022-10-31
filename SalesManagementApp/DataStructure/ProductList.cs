260ffc208457e8f0ddb85eabd19e446f1212319b
taannnleee <116615872+taannnleee@users.noreply.github.com>
2022-10-31 23:15:41 +0700

    #5 complete

--- SalesManagementApp/DataStructure/ProductList.cs
+++ SalesManagementApp/DataStructure/ProductList.cs
@@ -7,41 +7,69 @@ namespace SalesManagementApp.DataStructure
     {
         public ProductList(int size) : base(size)
         {
-        }
 
-        public override Product Get(int index)
-        {
-            return base.list_[index];
         }
 
         public override void AddItem(int index, Product value)
         {
+            if (base.iSize >= iCapacity) return;
+            for (int i = base.iSize; i > index; i--)
+            {
+                base.list_[i] = base.list_[i - 1];
+            }
+            iSize++;
             base.list_[index] = value;
         }
 
         public override void AddLast(Product item)
         {
-            throw new NotImplementedException();
+            if (base.iSize >= iCapacity) return;
+            base.list_[base.iSize++] = item;
         }
 
         public override void AddRange(ArrayList<Product> sourceList)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < sourceList.Size; i++)
+            {
+                if (base.iSize >= Capacity) return;
+
+                else
+                {
+                    base.list_[base.iSize++] = sourceList.Get(i);
+                }
+            }
+        }
+        public override Product Get(int index)
+        {
+            return base.list_[index];
         }
 
         public override void RemoveItem(int index)
         {
-            throw new NotImplementedException();
+            for (int i = index; i < base.iSize; i++)
+            {
+                base.list_[i] = base.list_[i + 1];
+            }
+            base.iSize--;
         }
 
         public override void Print()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < base.iSize; i++)
+            {
+                base.list_[i].xuat();
+            }
         }
 
         public override int SearchItem(Product item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < base.iSize; i++)
+            {
+                if (base.list_[i] == item)
+                    return i;
+            }
+            Console.WriteLine("TimKhongThay");
+            return -1;
         }
     }
 }
--- SalesManagementApp/Models/Date.cs
+++ SalesManagementApp/Models/Date.cs
@@ -3,11 +3,30 @@ namespace SalesManagementApp.Models
 {
     public class Date
     {
-        public Date()
+        
+        private int iDay;
+        private int iMonth;
+        private int iYear;
+        public int IDay
         {
-
-            // fields: (iDay: int, iMonth: int, iYear: int)
-
+            get { return iDay; }
+            set { iDay = value; }
+        }
+        public int IMonth
+        {
+            get { return iMonth; }
+            set { iMonth = value; }
+        }
+        public int IYear
+        {
+            get { return iYear; }
+            set{iYear = value;}
+        }
+          public Date(int iDay, int iMonth, int iYear)
+        {
+            IDay = iDay;
+            IMonth = iMonth;
+            IYear = iYear;
         }
     }
 }
