6f80488b5d27654c13025fcc6b331c73d3925f78
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2022-10-31 15:47:47 -0700

    Giao (#10)
    * #4 - complete
    
    * #4 - delete enum
    
    Co-authored-by: ZaoQuynh <96408507+ZaoQuynh@users.noreply.github.com>
    Co-authored-by: AnhAn <anhan@AnhAns-MacBook-Air.local>

---
+++ SalesManagementApp/DataStructure/ManagerList.cs
@@ -0,0 +1,70 @@
+﻿using SalesManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagementApp.DataStructure
+{
+    public class ManagerList : ArrayList<Manager>
+    {
+
+        public ManagerList(int iCapacity) : base(iCapacity) {}
+
+        ~ManagerList() { }
+
+        public override void AddItem(int index, Manager item)
+        {
+            if (base.iSize >= iCapacity) return;
+            for(int i = base.iSize; i > index ; i--)
+            {
+                base.list_[i] = base.list_[i - 1];
+            }
+            iSize++;
+            base.list_[index] = item;
+        }
+
+        public override void AddLast(Manager item)
+        {
+           if(base.iSize >= iCapacity) return;
+            base.list_[base.iSize++] = item;
+        }
+
+        public override void AddRange(ArrayList<Manager> sourceList)
+        {
+            for(int i = 0; i < sourceList.Size; i++)
+                AddLast(sourceList.Get(i));
+        }
+
+        public override Manager Get(int index)
+        {
+            return base.list_[index];
+        }
+
+        public override void Print()
+        {
+            for(int i = 0; i<base.iSize; i++)
+            {
+                base.list_[i].Print();
+            }
+        }
+
+        public override void RemoveItem(int index)
+        {
+            for(int i = index; i< base.iSize; i++)
+            {
+                base.list_[i] = base.list_[i + 1];
+            }
+            base.iSize--;
+        }
+
+        public override int SearchItem(Manager item)
+        {
+            for(int i = 0; i< base.iSize; i++)
+                if (item.IsEqual(base.list_[i]))
+                    return i;
+            return -1;
+        }
+    }
+}
