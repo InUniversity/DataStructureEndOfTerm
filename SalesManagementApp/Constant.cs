29b02f31f6b8e9efbf8b4b52bf848fdfaee406c0
AnhAn <anhan@AnhAns-MacBook-Air.local>
2022-10-31 22:29:24 +0700

    #8 - complete

---
+++ SalesManagementApp/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SalesManagementApp
+{
+    public static class Constant
+    {
+        public const int ARROW_UP = 38;
+        public const int ARROW_DOWN = 40;
+        public const int ARROW_RIGHT = 39;
+        public const int ARROW_LEFT = 37;
+        public const int BTN_ENTER = 13;
+    }
+}
+
