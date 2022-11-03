dbc9f51879f767cd763a7f1bba736f8ffc5fd814
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2022-11-03 09:44:21 -0700

    #13 - complete rebuild (#14)
    Co-authored-by: AnhAn <anhan@AnhAns-MacBook-Air.local>

---
+++ SalesManagementApp/Activities/CustomerManagementActivity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SalesManagementApp.Activities
+{
+    public static class CustomerManagementActivity
+    {
+
+        public static int RunActivity()
+        {
+            return -1;
+        }
+    }
+}
+
---
+++ SalesManagementApp/Activities/ProductManangementActivity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SalesManagementApp.Activities
+{
+    public static class ProductManangementActivity
+    {
+
+        public static int RunActivity()
+        {
+            return -1;
+        }
+    }
+}
+
