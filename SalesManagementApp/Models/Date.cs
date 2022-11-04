07f26168663a236dbc1b912ce04e5d95a33316e8
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2022-11-03 18:18:44 -0700

    #13 - add Person class, edit Date class (#18)
    Co-authored-by: AnhAn <anhan@AnhAns-MacBook-Air.local>

--- SalesManagementApp/Models/Date.cs
+++ SalesManagementApp/Models/Date.cs
@@ -7,26 +7,47 @@ namespace SalesManagementApp.Models
         private int iDay;
         private int iMonth;
         private int iYear;
+
         public int IDay
         {
             get { return iDay; }
             set { iDay = value; }
         }
+
         public int IMonth
         {
             get { return iMonth; }
             set { iMonth = value; }
         }
+
         public int IYear
         {
             get { return iYear; }
             set{iYear = value;}
         }
-          public Date(int iDay, int iMonth, int iYear)
+
+        public Date(int iDay, int iMonth, int iYear)
         {
             IDay = iDay;
             IMonth = iMonth;
             IYear = iYear;
+        }
+
+        // methods
+        public void Input()
+        {
+            Console.WriteLine("Enter Date:");
+            Console.Write("Day: ");
+            iDay = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Month: ");
+            iMonth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Year: ");
+            iYear = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static implicit operator string(Date date)
+        {
+            return date.iDay + "-" + date.iMonth + "-" + date.iYear;
         }
     }
 }
