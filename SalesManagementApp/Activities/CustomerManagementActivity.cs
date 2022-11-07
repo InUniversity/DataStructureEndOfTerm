2d3bbed2befd8c2ff88b8979eb7fd517193c4bcc
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2022-11-07 05:52:22 -0800

    #15 - customer list, implement linked list (#20)

--- SalesManagementApp/Activities/CustomerManagementActivity.cs
+++ SalesManagementApp/Activities/CustomerManagementActivity.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using SalesManagementApp.Database;
+using SalesManagementApp.DataStructure;
+using SalesManagementApp.Models;
+
 namespace SalesManagementApp.Activities
 {
     public static class CustomerManagementActivity
@@ -6,8 +10,132 @@ namespace SalesManagementApp.Activities
 
         public static int RunActivity()
         {
+            CustomerList customerList = CustomerData.customerList;
+            CustomerList tempList;
+            Customer tempCustomer = new Customer();
+            string tempName;
+            Date start = new Date(-1, -1, -1), end = new Date(-1, -1, -1);
+            bool over;
+            char choose;
+            int tempID;
+            do
+            {
+                over = false;
+                Console.Clear();
+                Console.Write("===========================MENU===========================\n");
+                Console.Write("|1. Add a customer                                       |\n");
+                Console.Write("|2. Delete customer with ID                              |\n");
+                Console.Write("|3. Display customer list                                |\n");
+                Console.Write("|4. Sort by LAST purchased date                          |\n");
+                Console.Write("|5. Find by date of purchase                             |\n");
+                Console.Write("|6. Find by ID                                           |\n");
+                Console.Write("|7. Find by Name                                         |\n");
+                Console.Write("|8. Write file                                           |\n");
+                Console.Write("|9. Read from file                                       |\n");
+                Console.Write("|0. Quit app                                             |\n");
+                Console.Write("===========================MENU===========================\n");
+                Console.Write("Choose: ");
+                choose = (char) Console.ReadKey().Key;
+                Console.WriteLine();
+
+                switch (choose)
+                {
+                    case '1':
+                        Console.WriteLine("Enter customer: ");
+                        tempCustomer.Input();
+                        if (customerList.SearchNode(tempCustomer) != null)
+                        {
+                            Console.WriteLine(Constant.DUPLICATED_MESSAGE);
+                            break;
+                        }
+                        customerList.AddLast(tempCustomer);
+                        customerList.Print();
+                        break;
+                    case '2':
+                        Console.Write("Enter ID: ");
+                        tempID = Convert.ToInt32(Console.ReadLine());
+                        tempCustomer = customerList.FindByID(tempID);
+                        if (tempCustomer == null)
+                        {
+                            Console.WriteLine(Constant.NOT_FOUND_MESSAGE);
+                            break;
+                        }
+                        customerList.Remove(tempCustomer);
+                        customerList.Print();
+                        break;
+                    case '3':
+                        if (customerList.IsEmpty())
+                        {
+                            Console.WriteLine(Constant.EMPTY_MESSAGE);
+                            break;
+                        }
+                        customerList.Print();
+                        break;
+                    case '4':
+                        customerList.SortByLastPurchaseDate();
+                        customerList.Print();
+                        break;
+                    case '5':
+                        Console.WriteLine("Enter start date:\n{");
+                        start.Input();
+                        Console.WriteLine("}");
+                        Console.WriteLine("Enter end date:\n{");
+                        end.Input();
+                        Console.WriteLine("}");
+                        tempList = customerList.FindByDateOfPurchase(start, end);
+                        tempList.Print();
+                        break;
+                    case '6':
+                        Console.Write("Enter ID: ");
+                        tempID = Convert.ToInt32(Console.ReadLine());
+                        tempCustomer = customerList.FindByID(tempID);
+                        if (tempCustomer == null)
+                        {
+                            Console.WriteLine(Constant.NOT_FOUND_MESSAGE);
+                            break;
+                        }
+                        tempCustomer.Print();
+                        break;
+                    case '7':
+                        Console.Write("Enter name: ");
+                        tempName = Console.ReadLine();
+                        tempList = customerList.FindByName(tempName);
+                        if (tempList.IsEmpty())
+                        {
+                            Console.WriteLine(Constant.EMPTY_MESSAGE);
+                            break;
+                        }
+                        tempList.Print();
+                        break;
+                    case '8':
+                        Console.WriteLine("Enter file name: ");
+                        tempName = Console.ReadLine();
+                        if (!customerList.WriteFile(tempName))
+                            Console.WriteLine(Constant.NOT_FOUND_MESSAGE);
+                        else
+                        {
+                            Console.WriteLine(Constant.SUCCESS_MESSAGE);
+                            customerList.Print();
+                        }
+                        break;
+                    case '9':
+                        tempName = "customer.txt";
+                        Console.WriteLine("File name: " + tempName);
+                        if (!customerList.AddFromFile(tempName))
+                            Console.WriteLine(Constant.NOT_FOUND_MESSAGE);
+                        else
+                        {
+                            Console.WriteLine(Constant.SUCCESS_MESSAGE);
+                            customerList.Print();
+                        }
+                        break;
+                    default:
+                        over = true;
+                        break;
+                }
+                Console.ReadKey();
+            } while (!over);
             return -1;
         }
     }
 }
-
---
+++ SalesManagementApp/Database/CustomerData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SalesManagementApp.DataStructure;
+using SalesManagementApp.Models;
+
+namespace SalesManagementApp.Database
+{
+    public static class CustomerData
+    {
+        public static CustomerList customerList = Init();
+
+        public static CustomerList Init()
+        {
+            CustomerList list_ = new CustomerList();
+            list_.AddLast(new Customer(90001, "Hoang Giang",
+                "Nữ", new Date(10, 10, 2003), "ap 1", 099999911,
+                14, 1333, "Vang", new Date(1, 1, 2022)));
+
+            list_.AddLast(new Customer(90002, "Le Thi Dieu",
+                "Nam", new Date(19, 1, 2000), "ap 5", 099999912,
+                9, 900, "Dong", new Date(1, 1, 2021)));
+
+            list_.AddLast(new Customer(90003, "Ly Nha",
+                "Nữ", new Date(10, 10, 2003), "ấp 10", 099999913,
+                4, 200, "Sat", new Date(10, 1, 2022)));
+
+            list_.AddLast(new Customer(90004, "Thien An",
+                "Nam", new Date(10, 9, 1999), "ấp 10", 099999914,
+                30, 1500, "bach kim", new Date(10, 9, 2022)));
+
+            list_.AddLast(new Customer(90005, "Thuy Dung",
+                "Nữ", new Date(10, 10, 2003), "ấp 10", 099999915,
+                16, 1231, "Vàng", new Date(8, 10, 2022)));
+
+            list_.AddLast(new Customer(90006, "Duy Khang",
+                "Nữ", new Date(10, 10, 2003), "ấp 10", 099999916,
+                15, 1300, "Vàng", new Date(1, 1, 2022)));
+            return list_;
+        }
+    }
+}
+
