240361dc3d52c1970d2378f6592a090d39097d8f
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2022-11-10 20:40:44 +0700

    Zao (#22)
    * #16 - complete
    
    * #16 - complete
    
    * #16 - complete
    
    * #16 - hotfix
    
    Co-authored-by: ZaoQuynh <96408507+ZaoQuynh@users.noreply.github.com>

---
+++ SalesManagementApp/Models/Employee.cs
@@ -0,0 +1,75 @@
+﻿using SalesManagementApp.Activities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagementApp.Models
+{
+    public class Employee : Person
+    {
+        protected int iSalary;
+        protected int iOrderNumber;
+        protected int iNoOfWork;
+  
+        // protected List<Customer> lCustomer;
+
+        //properties
+        public int SALARY
+        {
+            set { this.iSalary = value; }
+            get { return this.iSalary; }
+        }
+        public int ORDERNUMBER
+        {
+            set { this.iSalary = value; }
+            get { return this.iOrderNumber; }
+        }
+        public int NOOFWORK
+        {
+            set { this.iNoOfWork = value; }
+            get { return this.iNoOfWork; }
+        }
+
+        //constructor
+        public Employee():base()
+        {
+
+        }
+        public Employee(int id, string name, string sex, Date birthday, string address, int phoneno,int salary, int orderNumber, int noOfWork /*,List<Customer> lcustomer*/) :base(id,name,sex,birthday,address, phoneno)
+        {
+            this.iSalary = salary;
+            this.iOrderNumber = orderNumber;
+            this.iNoOfWork = noOfWork;
+            //this.lCustomer = lcostomer;
+        }
+
+        //destruction
+        ~Employee() { }
+
+        //method
+        public override void Input()
+        {
+            base.Input();
+            Console.WriteLine("Salary: "); this.iSalary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Totals Order Number: "); this.iOrderNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Totals No Of Work: "); this.iNoOfWork = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Salary: " + this.iSalary);
+            Console.WriteLine("Totals Order Number: "+this.iOrderNumber);
+            Console.WriteLine("Totals No Of Work: " +this.iNoOfWork);
+        }
+
+        public override bool IsEquals(Person person)
+        {
+            if(this.ID == person.ID)
+                return true;
+            return false;
+        }
+    }
+}
