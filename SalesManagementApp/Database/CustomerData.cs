b7a6ccca8a12e7b17514581e2586bff84ede966f
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2022-11-13 18:48:52 +0700

    #25 - Person class, CustomerData, Customer, CustomerList (#26)

--- SalesManagementApp/Database/CustomerData.cs
+++ SalesManagementApp/Database/CustomerData.cs
@@ -12,28 +12,28 @@ namespace SalesManagementApp.Database
         {
             CustomerList list_ = new CustomerList();
             list_.AddLast(new Customer(90001, "Hoang Giang",
-                "Nữ", new Date(10, 10, 2003), "ap 1", 099999911,
+                "Nu", new Date(10, 10, 2003), "ap 1", "099999911",
                 14, 1333, "Vang", new Date(1, 1, 2022)));
 
             list_.AddLast(new Customer(90002, "Le Thi Dieu",
-                "Nam", new Date(19, 1, 2000), "ap 5", 099999912,
+                "Nam", new Date(19, 1, 2000), "ap 5", "099999912",
                 9, 900, "Dong", new Date(1, 1, 2021)));
 
             list_.AddLast(new Customer(90003, "Ly Nha",
-                "Nữ", new Date(10, 10, 2003), "ấp 10", 099999913,
+                "Nu", new Date(10, 10, 2003), "ap 10", "099999913",
                 4, 200, "Sat", new Date(10, 1, 2022)));
 
             list_.AddLast(new Customer(90004, "Thien An",
-                "Nam", new Date(10, 9, 1999), "ấp 10", 099999914,
+                "Nam", new Date(10, 9, 1999), "ap 11", "099999914",
                 30, 1500, "bach kim", new Date(10, 9, 2022)));
 
             list_.AddLast(new Customer(90005, "Thuy Dung",
-                "Nữ", new Date(10, 10, 2003), "ấp 10", 099999915,
-                16, 1231, "Vàng", new Date(8, 10, 2022)));
+                "Nu", new Date(10, 10, 2003), "ap 10", "099999915",
+                16, 1231, "Vang", new Date(8, 10, 2022)));
 
             list_.AddLast(new Customer(90006, "Duy Khang",
-                "Nữ", new Date(10, 10, 2003), "ấp 10", 099999916,
-                15, 1300, "Vàng", new Date(1, 1, 2022)));
+                "Nu", new Date(10, 10, 2003), "ap 10", "099999916",
+                15, 1300, "Vang", new Date(1, 1, 2022)));
             return list_;
         }
     }
