75aea644af609c152ffd48b646dc84bfe86804ee
taannnleee <116615872+taannnleee@users.noreply.github.com>
2022-11-19 22:39:47 +0700

    #25  toi19/11

--- SalesManagementApp/Activities/ProductManangementActivity.cs
+++ SalesManagementApp/Activities/ProductManangementActivity.cs
@@ -1,6 +1,7 @@
 ﻿using SalesManagementApp.Database;
 using SalesManagementApp.DataStructure;
 using SalesManagementApp.Models;
+using SalesManagementApp.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -12,44 +13,74 @@ namespace SalesManagementApp.Activities
         public static int RunActivity()
         {
             ProductList productList = ProductData.productList;
-            ProductList temp = new ProductList(100);
-            Product tempProduct; 
-            int index;
             ProductList tempList = new ProductList(100);
+            Product tempProduct;
             Date today;
+            StringCustom tempName;
+            int index;
             int sum;
-            char choose;
+            int choose = 11;
             while (true)
             {
-                Console.Write("=========================MENU=========================\n");
-                Console.Write("|0. Check product in stock                           |\n");
-                Console.Write("|1. Enter a product                                  |\n");
-                Console.Write("|2. Delete product by index                          |\n");
-                Console.Write("|3. Add products to any position                     |\n");
-                Console.Write("|4. Search product by ID                             |\n");
-                Console.Write("|5. Search product by Name                           |\n");
-                Console.Write("|6. Sort By NumberOfProduct                          |\n");
-                Console.Write("|7. Check Expired Products                           |\n");
-                Console.Write("|8. Total products in stock                          |\n");
-                Console.Write("|9. Back                                             |\n");
-                Console.Write("|any key. Quit app                                   |\n");
-                Console.Write("=========================MENU=========================\n");
+                Console.Clear();
+
+                Printer.PrintGroupInformation(70);
+                Console.Write("=========================MENU==========================\n");
+                Console.Write("|0. Check product in stock                            |\n");
+                Console.Write("|1. Add products to the top of the list               |\n");
+                Console.Write("|2. Add products to the end of the list               |\n");
+                Console.Write("|3. Insert the product in any position                |\n");
+                Console.Write("|4. Delete product by index                           |\n");
+                Console.Write("|5. Delete product by ID                              |\n");
+                Console.Write("|6. Search product by ID                              |\n");
+                Console.Write("|7. Search product by Name                            |\n");
+                Console.Write("|8. Sort By NumberOfProduct                           |\n");
+                Console.Write("|9. Check Expired Products                            |\n");
+                Console.Write("|10. Total products in stock                          |\n");
+                Console.Write("|11. Back to Main activity                            |\n");
+                Console.Write("|any key. Quit app                                    |\n");
+                Console.Write("=========================MENU==========================\n");
                 Console.Write("Choose: ");
-                choose = Convert.ToChar(Console.ReadLine());
+                try
+                {
+                    choose = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine();
                 switch (choose)
                 {
-                    case '0':
+                    case 0:
                         Console.WriteLine("---------------------------------------------Result---------------------------------");
                         productList.Print();
                         break;
-                    case '1':
-                        tempProduct =new Product();
+                    case 1:
+                        tempProduct = new Product();
+                        tempProduct.Input();
+                        productList.AddFirst(tempProduct);
+                        Console.WriteLine("---------------------------------------------Result---------------------------------");
+                        productList.Print();
+                        break;
+                    case 2:
+                        tempProduct = new Product();
                         tempProduct.Input();
                         productList.AddLast(tempProduct);
                         Console.WriteLine("---------------------------------------------Result---------------------------------");
                         productList.Print();
                         break;
-                    case '2':
+                    case 3:
+                        tempProduct = new Product();
+                        Console.WriteLine("Location Add Products");
+                        index = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Product Information");
+                        tempProduct.Input();
+                        productList.AddItem(index, tempProduct);
+                        Console.WriteLine("---------------------------------------------Result---------------------------------");
+                        productList.Print();
+                        break;
+                    case 4:
                         Console.WriteLine("Enter the product location to delete.");
                         index = Convert.ToInt32(Console.ReadLine());
                         if (index >= productList.Size)
@@ -61,17 +92,15 @@ namespace SalesManagementApp.Activities
                         Console.WriteLine("---------------------------------------------Result---------------------------------");
                         productList.Print();
                         break;
-                    case '3':
+                    case 5:
                         tempProduct = new Product();
-                        Console.WriteLine("Location Add Products");
-                        index = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Product Information");
-                        tempProduct.Input();
-                        productList.AddItem(index, tempProduct);
+                        Console.WriteLine("Enter the product ID to delete.");
+                        tempProduct.ID = Convert.ToInt32(Console.ReadLine());
+                        productList.RemoveItemByID(tempProduct);
                         Console.WriteLine("---------------------------------------------Result---------------------------------");
                         productList.Print();
                         break;
-                    case '4':
+                    case 6:
                         tempProduct = new Product();
                         Console.WriteLine("Enter product ID");
                         tempProduct.ID = Convert.ToInt32(Console.ReadLine());
@@ -84,7 +113,7 @@ namespace SalesManagementApp.Activities
                             tempList.Print();
                         }
                         break;
-                    case '5':
+                    case 7:
                         tempProduct = new Product();
                         Console.WriteLine("Enter product name ");
                         tempProduct.Name = Console.ReadLine();
@@ -98,11 +127,11 @@ namespace SalesManagementApp.Activities
                             tempList.Print();
                         }
                         break;
-                    case '6':
+                    case 8:
                         productList.SortByNumberOfProduct();
                         productList.Print();
                         break;
-                    case '7':
+                    case 9:
                         today = new Date();
                         Console.WriteLine("Enter the current date");
                         today.Input();
@@ -114,14 +143,13 @@ namespace SalesManagementApp.Activities
                             tempList.Print();
                         }
                         break;
-                    case '8':
+                    case 10:
                         sum = productList.TotalGoods();
                         Console.WriteLine(sum);
                         break;
-                    case '9':
+                    case 11:
                         return 2;
                     default:
-                        Console.WriteLine(Constant.QUIT_APP_MESSAGE);
                         return -1;
                 }
                 Console.WriteLine("Enter To Continue");
