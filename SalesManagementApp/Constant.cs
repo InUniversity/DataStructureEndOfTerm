cf641847b4c742a259093a06f3de6405128c9a58
taannnleee <116615872+taannnleee@users.noreply.github.com>
2022-11-19 14:20:45 +0700

    #25   19/11

--- SalesManagementApp/Constant.cs
+++ SalesManagementApp/Constant.cs
@@ -8,11 +8,13 @@ namespace SalesManagementApp
         public const int RIGHT_ARROW = 39;
         public const int LEFT_ARROW = 37;
         public const int ENTER_KEY = 13;
+        public const int LINE_SYMBOL = 9552;
 
         public const string EMPTY_MESSAGE = "-> List is Empty!!!";
         public const string NOT_FOUND_MESSAGE = "-> Not found!!!";
         public const string DUPLICATED_MESSAGE = "-> Duplicated!!!";
         public const string SUCCESS_MESSAGE = "-> Success!!!";
+        public const string NOT_VALID_MESSAGE = "-> Not valid!!!";
 
         public const string NOT_FOUND_PRODUCT_MESSAGE = "-> No products found!!!";
         public const string NOT_OUT_OF_TIME_PRODUCT_MESSAGE = "-> No products are out of date!!!";
